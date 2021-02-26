2845ff942e7df3c59ab3bdd694dee2e5db665a71
ellabagzs <76996927+ellabagzs@users.noreply.github.com>
2021-02-27 02:59:38 +0800

    Add files via upload

---
+++ index (2).php
@@ -0,0 +1,15 @@
+<html>
+
+<frameset>
+<frameset cols="35,30,35">
+<frame src="part1.html">
+<frameset rows= 50,50>
+<frame src="part 2.html">
+<frame src="part3.html">
+</frameset>
+
+<frameset cols>
+<frame src="part4.html">
+</frameset>
+
+</html>
